e5c72c2d0385be6382ad63718981a8e8a2f44c72
Ash Pudasaini <67235783+ashshelby@users.noreply.github.com>
2021-05-22 19:13:59 +0545

    Update install.py
    A Script to install all the necessary modules for the program to run.

--- install.py
+++ install.py
@@ -1,1 +1,25 @@
+def makeInstall():
+  rootval = os.getuid()
+    if  str(rootval) != "0":
+        print("Please use this script as root, Permission Denied")
+        exit
+    else:
+        print("Installing necessary modules via pip3,make sure you have pip3 installed")
+        try:
+          subprocess.call(["pip3","install","scapy","scapy.layers","argparse"])
+         except:
+          print("Something went wrong, please manually install all pacakges")
 
+  
+
+try:
+  import os
+  import scapy.all
+  import scapt.layers
+  import argparse
+  import optparse
+  import time
+  import sys
+  import subprocess
+except ModuleNotFoundError:
+  makeInstall()
