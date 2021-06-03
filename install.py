10b2b4ebf0a81287d6186d4522b171f73a573fe5
Ash Pudasaini <67235783+ashshelby@users.noreply.github.com>
2021-06-03 15:22:40 +0545

    Update install.py

--- install.py
+++ install.py
@@ -1,14 +1,15 @@
 def makeInstall():
+  import os;
   rootval = os.getuid()
-   if  str(rootval) != "0":
-       print("Please use this script as root, Permission Denied")
-       exit
-   else:
-       print("Installing necessary modules via pip3,make sure you have pip3 installed")
-       try:
-         subprocess.call(["pip3","install","scapy","scapy.layers","argparse","colorama"])
-        except:
-          print("Something went wrong, please manually install all pacakges")
+  if  str(rootval) != "0" :
+      print("Please use this script as root, Permission Denied")
+      exit
+  else:
+      print("Installing necessary modules via pip3,make sure you have pip3 installed")
+      try:
+        subprocess.call(["pip3","install","scapy","scapy.layers","argparse","colorama"])
+      except:
+        print("Something went wrong, please manually install all pacakges")
 
   
 
